9e9476579f65761cd012d8a9c761ed2ca2eeca4a
Vlad <vhv1000@gmail.com>
2023-07-30 13:04:08 +0200

    added new list exercises

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ These are my Haskell notes.
 The notes are based from the lectures from Phillip Hagenlocher:
 [Playlist -- Learning Haskell for Imperative Programmers](https://www.youtube.com/watch?v=Vgu82wiiZ90&list=PLe7Ei6viL6jGp1Rfu0dil1JH1SHk9bgDV)
 )
+
--- no5_list_exercises.hs
+++ no5_list_exercises.hs
@@ -29,3 +29,37 @@ newNub [] = []
 newNub (x : xs)
   | newElem x xs = newNub xs
   | otherwise = x : newNub xs
+
+-- ex 3
+-- Create a function isAsc that returns True
+-- if the list given to it is a list of ascending order
+
+isAsc :: [Int] -> Bool
+isAsc [] = False
+isAsc (x : xs)
+  | length (x : xs) == 1 = True
+  | x < head xs = isAsc xs
+  | otherwise = False
+
+-- reference solution
+
+isAsc1 :: [Int] -> Bool
+-- 0 or 1 elements are true
+isAsc1 [] = True
+isAsc1 [x] = True
+isAsc1 (x : y : xs) =
+  (x <= y) && isAsc1 (y : xs)
+
+-- ex 4
+-- Directed Graphs
+-- Create a function hasPath that
+-- determines if a path from one node to
+-- another exists within a directed graph
+
+hasPath :: [(Int, Int)] -> Int -> Int -> Bool
+hasPath [] x y = x == y
+hasPath ed x y
+  | x == y = True
+  | otherwise =
+      let ed' = [(n, m) | (n, m) <- ed, n /= x]
+       in or [hasPath ed' m y | (n, m) <- ed, n == x]
